ba2852857f10e1c01d652a3c2551c3751d40a83a
Aaron Ogle <aaron@geekgonecrazy.com>
2019-11-28 00:02:38 -0600

    update example and add verbose flag

--- cmd/migrateFileStores/main.go
+++ cmd/migrateFileStores/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"log"
 
 	migratefiles "github.com/RocketChat/MigrateFileStore"
 	"github.com/RocketChat/MigrateFileStore/config"
@@ -11,14 +12,19 @@ func main() {
 
 	configFile := flag.String("configFile", "config.yaml", "Config File full path. Defaults to current folder")
 	storeName := flag.String("storeName", "Uploads", "Store Name.  Options: (Uploads, Avatars)")
-	method := flag.String("method", "migrate", "Migrate/Upload/Download")
+	method := flag.String("method", "download", "Migrate/Upload/Download")
 	skipErrors := flag.Bool("skipErrors", false, "Skip on error")
+	verbose := flag.Bool("verbose", true, "setting to true will spit out more verbose logs")
 
 	flag.Parse()
 
 	config, err := config.Load(*configFile)
 	if err != nil {
 		panic(err)
+	}
+
+	if *verbose {
+		config.DebugMode = true
 	}
 
 	migrate, err := migratefiles.New(config, *skipErrors)
@@ -32,14 +38,17 @@ func main() {
 
 	switch *method {
 	case "migrate":
+		log.Println("Beginning migration of files")
 		if err := migrate.MigrateStore(); err != nil {
 			panic(err)
 		}
 	case "upload":
+		log.Println("Beginning upload of files")
 		if err := migrate.UploadAll(config.TempFileLocation); err != nil {
 			panic(err)
 		}
 	case "download":
+		log.Println("Beginning download of files")
 		if err := migrate.DownloadAll(); err != nil {
 			panic(err)
 		}
@@ -47,5 +56,7 @@ func main() {
 		flag.Usage()
 		return
 	}
+
+	log.Println("Finished!")
 
 }
